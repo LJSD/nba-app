7201a0bfef78b1905d4035e5a79d5b8ce1bad169
Stephen Armstrong <starmstrong05@gmail.com>
2017-11-01 18:21:30 -0700

    now adding highlights page

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "alttimtal",
+  password: "0038ratdog",
   database: "userSignUp_db"
 });
 
