42923b3aa90067ff1a556df3cdd88d5fb58b2fa1
Luna <altynaylinstrum@yahoo.com>
2017-11-01 20:03:34 -0700

    Updated

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -15,8 +15,9 @@ function newAccount() {
         $.post("/api/new", newUser)
         // On success, run the following code
         .done(function(result) {
-            console.log(newUser); 
-            loginAttempt(newUser.email, newUser.password);
+            //console.log(newUser); 
+            // TODO --v this is broken
+            // loginAttempt(newUser.email, newUser.password);
         });
 
         // Empty each input box by replacing the value with an empty string
@@ -129,52 +130,12 @@ goBackToHome();
 
 //   if (data.length !== 0 && newUser) {
 //     for (var i = 0; i < data.length; i++) {
-//       console.log(newUser);
+//       //console.log(newUser);
 //     }
 //   }
 
 // });
 
 
-//PlayerStats Graphs
 
-//  var ctx = document.getElementById("graphs").getContext('2d');
-//  console.log("ctx is ",ctx)
-
-//  var graphs = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// });
 
