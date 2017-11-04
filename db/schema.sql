02bb9f629dce8d6b6c6dd23460b85af2e1991221
Luna <altynaylinstrum@yahoo.com>
2017-11-04 10:42:07 -0700

    Database Updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,3 @@
-CREATE DATABASE userSignUp_db;
-USE userSignUp_db;
 
-CREATE TABLE userInfo
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	email varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE playerAstrology
-(
-	id int NOT NULL AUTO_INCREMENT,
-	starSign varchar(255) NOT NULL,
-    description varchar(255) NOT NULL,
-	players varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
-SELECT * FROM playerAstrology;
+USE nba_stats;
 
